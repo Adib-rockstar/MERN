4bf8e74fe2f415d8722eac6f0d65a6e5eafb9563
Adib-rockstar <puthawalaadib@gmail.com>
2022-08-18 20:31:19 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import ReactDOM  from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import {legacy_createStore as createStore ,applyMiddleware ,compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducer';
 
 import App from './App';
 
+const store=createStore(reducers,compose(applyMiddleware(thunk)))
 ReactDOM.render(<App/>,document.getElementById('root'));
